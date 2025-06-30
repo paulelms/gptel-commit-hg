17df01a773828cec639164c784c2822a699f1b36
lakkiy <liubolovelife@gmail.com>
2025-06-30 11:33:15 +0800

    Update README and bump gptel dependency version
    Improved installation instructions for MELPA and added badges to the
    README. Updated the gptel dependency in the package metadata.
    
    * README.org: Refine installation steps, add badges, and improve docs.
    * gptel-commit.el: Bump `gptel` dependency to 0.9.8.

--- gptel-commit.el
+++ gptel-commit.el
@@ -3,8 +3,8 @@
 ;; Copyright (C) 2025 Liu Bo
 
 ;; Author: Liu Bo
-;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.1") (gptel "0.1.0"))
+;; Version: 0.1.1
+;; Package-Requires: ((emacs "27.1") (gptel "0.9.8"))
 ;; Keywords: vc, convenience
 ;; URL: https://github.com/lakkiy/gptel-commit
 
